61537a640f7cbd9256f842c3cb62e96f6312108d
LENOVO <LENOVO@LENOVO-PC>
2022-02-01 10:13:39 +0530

    FirstCommit

---
+++ src/test/java/hike/Sample1.java
@@ -0,0 +1,8 @@
+package hike;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		System.out.println("hiii");
+	}
+
+}
---
+++ src/test/java/hike/Sample2.java
@@ -0,0 +1,8 @@
+package hike;
+
+public class Sample2 {
+public static void main(String[] args) {
+	System.out.println("helloooooo");
+}
+
+}
